481a504be9d90484958ebe40385d4f2e19c1f0a5
Scalphax <scalphax@qiulishe.com>
2025-12-16 21:23:17 +0800

    解耦校验功能

--- game/wordle.go
+++ game/wordle.go
@@ -11,19 +11,28 @@ func Game(maxTurn int, wordList []string) bool {
 	var charConfirmed [26]int
 	randInt := rand.Intn(len(wordList))
 	answer := wordList[randInt]
+	fmt.Printf("%s", answer)
 	turn := 0
 	for turn < maxTurn {
-		var input []rune
+		var input string
 		fmt.Printf("[%d]Input your answer: ", turn+1)
 		fmt.Scanln(&input)
-		if valid := check(string(input), wordList); valid {
+		if valid := check(input, wordList); valid {
 			turn++
-			if correct := checkAnswer(string(input), answer, &charConfirmed); correct {
+			charState := checkAnswer(input, answer, &charConfirmed)
+			printCharState(charState, input)
+			printCharConfirmed(&charConfirmed)
+			correct := true
+			for _, i := range charState {
+				if i != 3 {
+					correct = false
+					break
+				}
+			}
+			if correct {
 				fmt.Printf("Correct!\n")
 				return true
 			}
-		} else {
-			continue
 		}
 	}
 	fmt.Printf("Game over! The answer is: %s\n", answer)
@@ -65,10 +74,11 @@ func compare(input string, wordList string) int {
 	return 0
 }
 
-func checkAnswer(input string, answer string, charConfirmed *[26]int) bool {
+func checkAnswer(input string, answer string, charConfirmed *[26]int) []byte {
 	var charCount [26]int
 	var correctCount = 0
 	wordLen := len(answer)
+	charState := make([]byte, wordLen)
 	for i := 0; i < wordLen; i++ {
 		if input[i] != answer[i] {
 			charCount[answer[i]-'a']++
@@ -76,21 +86,42 @@ func checkAnswer(input string, answer string, charConfirmed *[26]int) bool {
 	}
 	for i := 0; i < wordLen; i++ {
 		if input[i] == answer[i] {
-			fmt.Printf("\033[42m%c\033[0m", input[i])
+			//fmt.Printf("\033[42m%c\033[0m", input[i])
+			charState[i] = 3
 			charConfirmed[input[i]-'a'] = 3
 			correctCount++
 		} else {
 			if charCount[input[i]-'a'] > 0 {
 				charCount[input[i]-'a']--
-				fmt.Printf("\033[43m%c\u001B[0m", input[i])
+				//fmt.Printf("\033[43m%c\u001B[0m", input[i])
+				charState[i] = 2
 				charConfirmed[input[i]-'a'] = max(charConfirmed[input[i]-'a'], 2)
 			} else {
-				fmt.Printf("\033[100m%c\u001B[0m", input[i])
+				//fmt.Printf("\033[100m%c\u001B[0m", input[i])
+				charState[i] = 1
 				charConfirmed[input[i]-'a'] = max(charConfirmed[input[i]-'a'], 1)
 			}
 		}
 	}
-	fmt.Printf("\nKnown: ")
+	return charState
+}
+
+func printCharState(charState []byte, word string) {
+	for i, char := range word {
+		switch charState[i] {
+		case 1:
+			fmt.Printf("\033[100m%c\u001B[0m", char)
+		case 2:
+			fmt.Printf("\033[43m%c\u001B[0m", char)
+		case 3:
+			fmt.Printf("\033[42m%c\033[0m", char)
+		}
+	}
+	fmt.Printf("\n")
+}
+
+func printCharConfirmed(charConfirmed *[26]int) {
+	fmt.Printf("Known: ")
 	for i := 0; i < 26; i++ {
 		switch charConfirmed[i] {
 		case 0:
@@ -104,9 +135,4 @@ func checkAnswer(input string, answer string, charConfirmed *[26]int) bool {
 		}
 	}
 	fmt.Printf("\n")
-	if correctCount == wordLen {
-		return true
-	} else {
-		return false
-	}
 }
