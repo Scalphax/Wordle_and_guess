e05fd39ff6c4604fda373a22ea47b3b11cef7ea4
Scalphax <scalphax@qiulishe.com>
2025-12-17 20:30:32 +0800

    新增模式选择

--- main.go
+++ main.go
@@ -3,13 +3,15 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"math/rand"
 	"os"
 	"time"
+	"wordle/game"
 	"wordle/guessv2"
 )
 
 func main() {
-	f, _ := os.Open("word_list.txt")
+	f, _ := os.Open("2k_word_list.txt")
 	defer f.Close()
 
 	var wordList []string
@@ -18,26 +20,64 @@ func main() {
 	for scanner.Scan() {
 		wordList = append(wordList, scanner.Text())
 	}
-	turnSum := 0
-	gameRound := len(wordList)
-	start := time.Now()
-	guessv2.NewSolver(wordList)
-	//for _, answer := range wordList {
-	//	//randInt := rand.Intn(len(wordList))
-	//	//answer := wordList[randInt]
-	//	turnSum += game.Auto(answer, wordList)
-	//}
-	time := time.Since(start)
-	fmt.Printf("avgTurn: %f, avgTime: %fs", float32(turnSum)/float32(gameRound), time.Seconds()/float64(gameRound))
+	fmt.Printf("0.Play 1.Robot 2.Full word list benchmark 3+.Random benchmark(int for turn)\nSelect mode: ")
+	mode := 0
+	fmt.Scanf("%d", &mode)
 
-	//var maxTurn = 5
-	//print("Input max turn: ")
-	//_, err := fmt.Scanf("%d\n", &maxTurn)
-	//if err != nil {
-	//	fmt.Printf("%v\n", err)
-	//}
-	//
-	//for {
-	//	game.Game(maxTurn, wordList)
-	//}
+	run(mode, wordList)
+}
+
+func run(mode int, wordList []string) {
+	switch mode {
+	case 0:
+		var maxTurn = 5
+		print("Input max turn: ")
+		_, err := fmt.Scanf("%d\n", &maxTurn)
+		if err != nil {
+			fmt.Printf("%v\n", err)
+		}
+
+		for {
+			game.Game(maxTurn, wordList)
+		}
+	case 1:
+		s := guessv2.NewSolver(wordList)
+		charaState := make([]byte, len(wordList[0]))
+		for {
+			fmt.Printf("%s\n", s.MakeChoice(charaState))
+			var input string
+			fmt.Scanln(&input)
+			for i, num := range input {
+				charaState[i] = byte(num - '0')
+			}
+			if input == "22222" {
+				s.Reset()
+			}
+		}
+	case 2:
+		turnSum := 0
+		gameRound := len(wordList)
+		start := time.Now()
+		s := guessv2.NewSolver(wordList)
+
+		for _, answer := range wordList {
+			turnSum += game.Auto(answer, wordList, s)
+		}
+
+		totalTime := time.Since(start)
+		fmt.Printf("avgTurn: %f, avgTime: %fs", float32(turnSum)/float32(gameRound), totalTime.Seconds()/float64(gameRound))
+
+	default:
+		turnSum := 0
+		gameRound := mode
+		start := time.Now()
+		s := guessv2.NewSolver(wordList)
+		for i := 0; i < gameRound; i++ {
+			randInt := rand.Intn(len(wordList))
+			answer := wordList[randInt]
+			turnSum += game.Auto(answer, wordList, s)
+		}
+		totalTime := time.Since(start)
+		fmt.Printf("avgTurn: %f, avgTime: %fs", float32(turnSum)/float32(gameRound), totalTime.Seconds()/float64(gameRound))
+	}
 }
